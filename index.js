c96461a149a980ee2ce730b6ce00f73c27c4db99
vaishsh <vaishali.shah2010@gmail.com>
2018-05-01 22:52:18 -0500

    changes to fix socket.io connection

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 'use strict'
 
 const express = require('express');
-const initHttpServer = require('./src/api')
+const http = require('http');
+const port = 9001
+// const initHttpServer = require('./src/api')
 
 const APIAI_TOKEN = "754774bf5754433dbbc691c4436abcd0"
 const APIAI_SESSION_ID = "12asdlashr34sfsd"
+
+
 const app = express();
 
 app.use(express.static(__dirname + '/views')); // html
 app.use(express.static(__dirname + '/public')); // js, css, images
 
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+})
+
+const server = http.createServer(app);
+server.listen(port);
+
 const apiai = require('apiai')(APIAI_TOKEN);
 
-const io = require('socket.io')(initHttpServer(app))
+const io = require('socket.io').listen(server)
 
 io.on('connection', function(socket) {
   socket.on('chat message', (text) => {
